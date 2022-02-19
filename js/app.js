f086ba8b69660c34c0efe8d3dfada92fa27a11f9
Erick Rodrigues <profissional.erick@gmail.com>
2022-02-19 19:23:23 -0300

    terceiro

--- js/app.js
+++ js/app.js
@@ -5,10 +5,37 @@ const btnMenuClose = document.querySelector('#btnMenuClose');
 const cart = document.querySelector('.cart');
 const btnCart = document.querySelector('.btnCart')
 
+const btnPlus = document.querySelector('#btnPlus');
+const btnMinus = document.querySelector('#btnMinus');
+const productCounter = document.querySelector('.counter')
+
+const gallery = document.querySelector('pic');
+const heroImg = document.querySelector('.product-hero');
+
+const btnNext = document.querySelector('.next');
+const btnPrevious = document.querySelector('.previous');
+
+let productCounterValue = 1;
+
 btnHamburguer.addEventListener('click', onHamburguerClick);
 btnMenuClose.addEventListener('click', onBtnCloseMenuClick );
 
 btnCart.addEventListener('click', openCart);
+
+btnPlus.addEventListener('click', productCounterPlus);
+btnMinus.addEventListener('click', productCounterMinus);
+
+gallery.forEach(img => {
+    img.addEventListener('click', onThumbClick)
+});
+
+btnNext.addEventListener('click', handleBtnClickNext);
+btnPrevious.addEventListener('click', handleBtnClickPrevious);
+
+
+
+
+
 
 function onHamburguerClick() {
     menu.classList.remove('hidden');
@@ -22,3 +49,62 @@ function openCart() {
     cart.classList.toggle('hidden');
 }
 
+function productCounterPlus() {
+    //console.log(productCounterValue);
+    setProductCounter(1);
+}
+
+function productCounterMinus() {
+    setProductCounter(-1);
+}
+
+function setProductCounter(value) {
+    if ((productCounterValue + value) > 0) {
+        productCounterValue += value;
+        productCounter.innerHTML = productCounterValue;
+    }
+    
+}
+
+function onThumbClick() {
+    gallery.forEach(img => {
+        img.classList.remove('active');
+    });
+
+    event.target.parentElement.classList.add('active');
+
+    heroImg.src = event.target.src.replace('-thumbnail', '');
+}
+
+function handleBtnClickNext(){
+    let imageIndex = getCurrentImageIndex();
+    imageIndex++;
+    if (imageIndex > 4) {
+        imageIndex = 1;
+    }
+    setHeroImage(imageIndex);
+}
+
+function handleBtnClickPrevious() {
+    let imageIndex = getCurrentImageIndex();
+    imageIndex--;
+    if (imageIndex < 1) {
+        imageIndex = 4;
+    }
+    setHeroImage(imageIndex);
+}
+
+function getCurrentImageIndex() {
+    const imageIndex = parseInt(heroImg.src.split('\\').pop().split('/').pop().replace('.jpg', '').replace('image-product-', ''));
+    return imageIndex;
+}
+
+function setHeroImage(imageIndex) {
+    heroImg.src = `./images/image-product-${imageIndex}.jpg`;
+    //images are not sync
+    gallery.forEach(img => {
+        img.classList.remove('active');
+    });
+    //set active thumbnail
+    gallery[imageIndex-1].classList.add('active');
+}
