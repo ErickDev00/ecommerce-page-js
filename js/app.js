c49cf7c56431f8b997f6ec372e16d41917fecced
Erick Rodrigues <profissional.erick@gmail.com>
2022-02-19 00:44:04 -0300

    second

--- js/app.js
+++ js/app.js
@@ -2,9 +2,13 @@ const menu = document.querySelector('.menu');
 const btnHamburguer = document.querySelector('.hamburguer');
 const btnMenuClose = document.querySelector('#btnMenuClose');
 
-btnHamburguer.addEventListener('click', onHamburguerClick);
+const cart = document.querySelector('.cart');
+const btnCart = document.querySelector('.btnCart')
 
+btnHamburguer.addEventListener('click', onHamburguerClick);
 btnMenuClose.addEventListener('click', onBtnCloseMenuClick );
+
+btnCart.addEventListener('click', openCart);
 
 function onHamburguerClick() {
     menu.classList.remove('hidden');
@@ -12,5 +16,9 @@ function onHamburguerClick() {
 
 function onBtnCloseMenuClick() {
     menu.classList.add('hidden');
+}
+
+function openCart() {
+    cart.classList.toggle('hidden');
 }
 
