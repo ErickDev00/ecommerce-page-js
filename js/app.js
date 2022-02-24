a22a9594968c2f34238fdade2c035db2bef7ec0f
Erick Rodrigues <profissional.erick@gmail.com>
2022-02-24 17:18:08 -0300

    final

--- js/app.js
+++ js/app.js
@@ -15,7 +15,29 @@ const heroImg = document.querySelector('.product-hero');
 const btnNext = document.querySelector('.next');
 const btnPrevious = document.querySelector('.previous');
 
+const btnAddToCard = document.querySelector('.btn');
+const cartCount = document.querySelector('.cart-count');
+const productInShoppingCart = document.querySelector('.products-in-cart');
+
+const msgEmpty = document.querySelector('.msg-empty');
+const checkout = document.querySelector('.checkout');
+
+const overlay = document.querySelector('.overlay');
+const lightbox = document.querySelector('.lightbox');
+
+
+let lightboxGallery;
+let lightboxHero;
+
+
+
 let productCounterValue = 1;
+let productsInCart = 0;
+let price = 250.0;
+let discount = 0.5;
+
+
+
 
 btnHamburguer.addEventListener('click', onHamburguerClick);
 btnMenuClose.addEventListener('click', onBtnCloseMenuClick );
@@ -33,6 +55,8 @@ gallery.forEach(img => {
 
 btnNext.addEventListener('click', handleBtnClickNext);
 btnPrevious.addEventListener('click', handleBtnClickPrevious);
+
+btnAddToCard.addEventListener('click', addToCart);
 
 
 
@@ -109,4 +133,159 @@ function setHeroImage(imageIndex) {
     });
     //set active thumbnail
     gallery[imageIndex-1].classList.add('active');
+}
+
+
+function addToCart() {
+    productsInCart += productCounterValue;
+
+    const productHTMLElement = `
+    <div class="item">
+        <img class="product-img" src="./images/image-product-1-thumbnail.jpg" alt="product 1 thumb">
+        <div class="details">
+            <div class="product-name">Autumn Limited Edition...</div>
+            <div class="price-group">
+                <div class="price">$${(price*discount).toFixed(2)}</div> x
+                <div class="count">${productsInCart}</div>
+                <div class="total-amount">$${(price*discount*productsInCart).toFixed(2)}</div>
+        </div>
+        </div>
+        <img id="btnDelete" src="./images/icon-delete.svg" alt="icon delete">
+    </div>
+    `;
+
+    productInShoppingCart.innerHTML = productHTMLElement;
+
+    updateCart();
+
+    const btnDelete = document.querySelector('#btnDelete');
+    btnDelete.addEventListener('click', onBtnDeleteClick);
+    //console.log(productsInCart);
+}
+
+function updateCart() {
+    updateCartIcon();
+    updateMsgEmpty();
+    updateCheckoutButton();
+}
+
+function updateCartIcon() {
+    cartCount.textContent = productsInCart;
+    if (productsInCart == 0) {
+        if (!cartCount.classList.contains('hidden')) {
+            cartCount.classList.add('hidden');
+        }
+    } else {
+        cartCount.classList.remove('hidden');
+    }
+}
+
+function updateMsgEmpty() {
+    if (productsInCart == 0) {
+        if (msgEmpty.classList.contains('hidden')) {
+            msgEmpty.classList.remove('hidden');
+        }
+    } else {
+        if (!msgEmpty.classList.contains('hidden')){
+            msgEmpty.classList.add('hidden');
+        }
+    }
+
+}
+
+function updateCheckoutButton() {
+    if (productsInCart == 0) {
+        if (!checkout.classList.contains('hidden')) {
+            checkout.classList.add('hidden');
+        }
+    } else {
+        checkout.classList.remove('hidden');
+    }
+}
+
+function onBtnDeleteClick() {
+    productsInCart--;
+    updateCart();
+
+    const el = document.querySelector('.count');
+    const totalAmount = document.querySelector('.total-amount');
+    el.innerHTML = productsInCart;
+    totalAmount.innerHTML = `$${(price*discount*productsInCart).toFixed(2)}`;
+
+    if (productsInCart == 0) {
+        productInShoppingCart.innerHTML = '';
+    }
+}
+
+function onHeroImgClick() {
+    if (window.innerWidth >= 1280) {
+        if (overlay.childElementCount == 1) {
+            const newNode = lightbox.cloneNode(true);
+            overlay.appendChild(newNode);
+
+            const btnOverlayClose = document.querySelector('#btnOverlayClose');
+            btnOverlayClose.addEventListener('click', onBtnOverlayClose);
+
+            lightboxGallery = overlay.querySelectorAll('.pic');
+            lightboxHero = overlay.querySelector('.product-hero');
+            lightboxGallery.forEach(img => {
+                img.addEventListener('click', onThumbClickLightbox);
+            });
+
+            const btnOverlayNext = overlay.querySelector('.next');
+            const btnOverlayPrevious = overlay.querySelector('.previous');
+            btnOverlayNext.addEventListener('click', handleBtnClickNextOverlay);
+            btnOverlayPrevious.addEventListener('click', handleBtnClickPreviousOverlay);
+        }
+        overlay.classList.remove('hidden');
+    }
+}
+
+function onBtnOverlayClose() {
+    overlay.classList.add('hidden');
+}
+
+function onThumbClickLightbox(event) {
+    //clear active state for all thumbnails
+    lightboxGallery.forEach(img => {
+        img.classList.remove('active');
+    });
+    //set active thumbnail
+    event.target.parentElement.classList.add('active');
+    //update hero image
+    lightboxHero.src = event.target.src.replace('-thumbnail', '');
+}
+
+
+function handleBtnClickNext() {
+    let imageIndex = getOverlayCurrentImageIndex();
+    imageIndex++;
+    if (imageIndex > 4) {
+        imageIndex = 1;
+    }
+    setOverlayHeroImage(imageIndex);
+}
+
+function handleBtnClickPrevious() {
+    let imageIndex = getOverlayCurrentImageIndex();
+    imageIndex--;
+    if (imageIndex < 1) {
+        imageIndex = 4;
+    }
+    setOverlayHeroImage(imageIndex);
+}
+
+function getOverlayCurrentImageIndex() {
+    const imageIndex = parseInt(lightboxHero.src.split('\\').pop().split('/').pop().replace('.jpg', '').replace('image-product-', ''));
+    return imageIndex;
+}
+
+function setOverlayHeroImage(imageIndex) {
+    lightboxHero.src = `./images/image-product-${imageIndex}.jpg`;
+    //images are not sync
+    lightboxGallery.forEach(img => {
+        img.classList.remove('active');
+    });
+    //set active thumbnail
+    lightboxGallery[imageIndex-1].classList.add('active');
 }
